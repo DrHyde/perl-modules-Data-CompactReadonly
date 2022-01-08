7f1c1e4f3b8a5612ebd1083ee356e516dc4ac972
David Cantrell <david@cantrell.org.uk>
2022-01-08 17:22:28 +0000

    book-keeping for release

--- lib/Data/CompactReadonly.pm
+++ lib/Data/CompactReadonly.pm
@@ -7,7 +7,7 @@ use Data::CompactReadonly::V0::Node;
 
 # Yuck, semver. I give in, the stupid cult that doesn't understand
 # what the *number* bit of *version number* means has won.
-our $VERSION = '0.0.6';
+our $VERSION = '0.1.0';
 
 =head1 NAME
 
